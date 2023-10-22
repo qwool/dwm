8cf9c0d2efc765adba2ac8f147a624a156ca41b0
qwool <nobody@example.com>
2023-10-22 19:42:08 +0300

    preserve on restart

--- restart.sh
+++ restart.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 dwmdir="$HOME/sl/dwm/dwm"
-while true; do
-    # Log stderror to a file 
-    $dwmdir 2> ~/.dwm.log
-    # No error logging
-    #dwm >/dev/null 2>&1
-done
+while type $dwmdir >/dev/null ; do $dwmdir && continue || break ;done
